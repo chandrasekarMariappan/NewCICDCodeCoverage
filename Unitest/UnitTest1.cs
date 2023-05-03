082d29fa68c7e1f07c1d92db682ff8f4be2b62f9
chandrasekarMariappan <chandrumca11@gmail.com>
2023-05-03 19:30:01 +0530

    Unit test case added

--- Unitest/UnitTest1.cs
+++ Unitest/UnitTest1.cs
@@ -15,31 +15,31 @@ namespace Unitest
             Assert.AreEqual(result, result);
         }
 
-        //[TestMethod]
-        //public void TestMethod2()
-        //{
-        //    var wfc = new WeatherForecastController();
-        //    var result = wfc.Get(4);
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var wfc = new WeatherForecastController();
+            var result = wfc.Get(4);
 
-        //    Assert.AreEqual(result.First().Date, result.First().Date);
-        //}
+            Assert.AreEqual(result.First().Date, result.First().Date);
+        }
 
-        //[TestMethod]
-        //public void TestMethod3()
-        //{
-        //    var wfc = new WeatherForecastController();
-        //    var result = wfc.Get(4);
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var wfc = new WeatherForecastController();
+            var result = wfc.Get(4);
 
-        //    Assert.AreEqual(result.First().Summary, result.First().Summary);
-        //}
+            Assert.AreEqual(result.First().Summary, result.First().Summary);
+        }
 
-        //[TestMethod]
-        //public void TestMethod4()
-        //{
-        //    var wfc = new WeatherForecastController();
-        //    var result = wfc.Get(4);
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var wfc = new WeatherForecastController();
+            var result = wfc.Get(4);
 
-        //    Assert.AreEqual(result.First().TemperatureC, result.First().TemperatureC);
-        //}
+            Assert.AreEqual(result.First().TemperatureC, result.First().TemperatureC);
+        }
     }
 }
